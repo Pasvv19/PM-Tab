fadaa24d6e1c316cf856dcb3d4c6c7bf03e30344
Pasvv19 <78598850+Pasvv19@users.noreply.github.com>
2022-02-04 20:53:25 +0100

    Icoontjes toegevoegd

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ function showTime(){
     var date = new Date();
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
-    // var s = date.getSeconds(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
 
     if (m < 10){
         m = '0' + m
     }
 
-    var time = h + ":" + m
+    if (s < 10){
+        s = '0' + s
+    }
+
+    var time = h + ":" + m + ":" + s
     document.getElementById("headline").innerText = time;
     document.getElementById("headline").textContent = time;
     
