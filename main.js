b5562a91d58ec51d28b6fe3eac2f940648debfeb
Pasvv19 <78598850+Pasvv19@users.noreply.github.com>
2022-02-07 12:20:40 +0100

    Extra files + clock speed increased

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function showTime(){
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
+    var ms = date.getMilliseconds(); // 0 - 999
 
     if (m < 10){
         m = '0' + m
@@ -12,11 +13,19 @@ function showTime(){
         s = '0' + s
     }
 
-    var time = h + ":" + m + ":" + s
+    if (ms < 10){
+        ms = '0' + ms
+    }
+
+    if (ms < 100){
+        ms = '0' + ms
+    }
+
+    var time = h + ":" + m + ":" + s + ":" + ms
     document.getElementById("headline").innerText = time;
     document.getElementById("headline").textContent = time;
     
-    setTimeout(showTime, 1000);
+    setTimeout(showTime, 1);
     
 }
 
